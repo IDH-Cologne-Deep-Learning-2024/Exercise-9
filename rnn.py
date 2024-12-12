f5074276d0d4506a0d46d86404ffd61018529256
javi1337 <jan@pulsarstudio.de>
2024-12-12 02:10:21 +0100

    My Exercise-9 solution with one error. FFNN is not working.

--- rnn.py
+++ rnn.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Embedding, Flatten, Input, Dense, Dropout, SimpleRNN, Bidirectional
+from tensorflow.keras.layers import Embedding, Flatten, Input, Dense, Dropout, SimpleRNN, Bidirectional, LSTM
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.optimizers import SGD, Adam
 from tensorflow.keras.regularizers import L1, L2, L1L2
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
@@ -29,3 +30,67 @@ def to_number(labels):
 
 
 df = pd.read_csv("names.csv", sep=",")
+df['nationality'] = to_number(df['nationality'])
+
+X = df["name"]
+y = df["nationality"]
+
+tokenizer = Tokenizer(char_level=True)
+tokenizer.fit_on_texts(X)
+vocab_size = len(tokenizer.word_index)+1
+max_name_length = max([len(name) for name in X])
+num_classes = len(set(y))
+X = tokenizer.texts_to_sequences(X)
+X = pad_sequences(X, padding="post")
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+#RNN
+RNN = Sequential([
+    Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=50),
+    SimpleRNN(128, activation="tanh",return_sequences=False),
+    Dropout(0.5),
+    Dense(64, activation="relu"),
+    Dense(num_classes, activation="softmax")
+])
+
+RNN.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+RNN.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+y_pred = RNN.predict(X_test)
+y_pred = y_pred.argmax(axis=1)
+print(classification_report(y_test, y_pred))
+
+#BiRNN
+BIRNN = Sequential([
+    Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=50, input_length=X.shape[1]),
+    Bidirectional(LSTM(units=128, return_sequences=False)),
+    Flatten(),
+    Dense(64, activation="relu"),
+    Dropout(0.3),
+    Dense(num_classes, activation="softmax")
+])
+
+BIRNN.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+BIRNN.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+y_pred = BIRNN.predict(X_test)
+y_pred = y_pred.argmax(axis=1)
+print(classification_report(y_test, y_pred))
+
+#FFNN
+FFNN = Sequential([
+    Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=100),
+    Dense(128, activation="relu"),
+    Dropout(0.5),
+    Dense(64, activation="relu"),
+    Dense(num_classes, activation="softmax")
+])
+
+FFNN.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+FFNN.fit(X_train, y_train), epochs=10, batch_size=32, validation_split=0.2)
+
+y_pred = FFNN.predict(X_test)
+y_pred = y_pred.argmax(axis=1)
+print(classification_report(y_test, y_pred))
